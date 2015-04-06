9e97c50818de03dfedbc81b0f93a218d4e7e60e0
maxime <maxime.pichou@gmail.com>
2015-04-06 15:10:06 +0200

    First commit of the project

---
+++ Jarret/src/upem/jarret/worker/ClientJarret.java
@@ -0,0 +1,87 @@
+package upem.jarret.worker;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
+import java.util.LinkedList;
+
+//http://www.journaldev.com/2324/jackson-json-processing-api-in-java-example-tutorial
+//http://repo1.maven.org/maven2/com/fasterxml/jackson/
+//http://docs.oracle.com/javase/7/docs/api/java/net/URLClassLoader.html#URLClassLoader(java.net.URL[],%20java.lang.ClassLoader)
+public class ClientJarret {
+	private final String clientID;
+	private InetSocketAddress serverAddress;
+	private final SocketChannel sc;
+	private static final int BUFFER_SIZE = 4096;
+	private ByteBuffer buff;
+	public static final Charset ASCII_CHARSET = Charset.forName("ASCII");
+
+	public ClientJarret(String clientID, String adressServer, int portServer) throws IOException {
+		this.clientID = clientID;
+		sc = SocketChannel.open();
+		serverAddress = new InetSocketAddress(adressServer, portServer);
+		//connection to server
+		sc.connect(serverAddress);
+	}
+
+	private static void usage() {
+		System.out.println("ClientJarret clientID Adress Port");
+
+	}
+	
+	public ByteBuffer getRequestPacket(){
+		buff = ByteBuffer.allocate(BUFFER_SIZE);
+		String request = "GET Task HTTP/1.1\r\n" + "Host: "+serverAddress.getHostString() + "\r\n" + "\r\n";
+		System.out.println(request);
+		buff.put(ASCII_CHARSET.encode(request));
+		return buff;
+	}
+
+	public void sendPacket(ByteBuffer buff) throws IOException{
+    	buff.flip();
+    	sc.write(buff);
+    	buff.clear();
+	}
+	
+	
+	
+	public static void main(String[] args) throws IOException {
+		if (args.length != 3) {
+			usage();
+			return;
+		}
+		String clientID = args[0];
+		String adress = args[1];
+		int port = Integer.valueOf(args[2]);
+		while(true){
+			ClientJarret cJarret = new ClientJarret(clientID, adress, port);
+			cJarret.sendPacket(cJarret.getRequestPacket());
+			HTTPReader reader = new HTTPReader(cJarret.sc, cJarret.buff);
+			HTTPHeader header = reader.readHeader();
+			System.out.println(header.getFields());
+			cJarret.buff.clear();
+			cJarret.buff = reader.readBytes(header.getContentLength());
+			ClientTask cTask = ClientTask.create(cJarret.buff, header);
+			int sleep;
+			if((sleep = cTask.haveToSleep()) != 0){
+				long timeSlept = System.currentTimeMillis();
+				while(System.currentTimeMillis() - timeSlept < sleep*1000){
+					try {
+						Thread.sleep(sleep * 1000 - (System.currentTimeMillis() - timeSlept));
+					} catch (InterruptedException e) {
+						//Do nothing
+					}
+				}
+			}
+			else{
+				cTask.doWork();
+			}
+		}
+		
+		
+		
+	}
+
+}
---
+++ Jarret/src/upem/jarret/worker/ClientTask.java
@@ -0,0 +1,103 @@
+package upem.jarret.worker;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.Objects;
+
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
+
+public class ClientTask {
+	private long jobId;
+	private String workerVersion;
+	private String workerURL;
+	private String workerClassName;
+	private int task;
+	private int secondsToSleep;
+	
+	public ClientTask(long jobId, String workerVersion, String workerURL, String workerClassName, int task) {
+		if(jobId < 0){
+			throw new IllegalArgumentException("JobId is not valid"); 
+		}
+		if(task < 0){
+			throw new IllegalArgumentException("task is not valid");
+		}
+		this.jobId = jobId;
+		this.workerVersion = Objects.requireNonNull(workerVersion);
+		this.workerURL = Objects.requireNonNull(workerURL);
+		this.workerClassName = Objects.requireNonNull(workerClassName);
+		this.task = task;
+		secondsToSleep = 0;
+	}
+	
+	public ClientTask(int secondsToSleep) {
+		if(secondsToSleep <= 0){
+			throw new IllegalArgumentException();
+		}
+		this.secondsToSleep = secondsToSleep;
+	}
+	
+	public int haveToSleep(){
+		return secondsToSleep;
+	}
+
+	public static ClientTask create(ByteBuffer buff, HTTPHeader header) {
+		if(!"application/json".equals(header.getContentType())){
+			throw new IllegalArgumentException();
+		}
+		String content = header.getCharset().decode(buff).toString();
+		JsonFactory jfactory = new JsonFactory();
+		long jobId = -1;
+		String workerVersion = "";
+		String workerURL = "";
+		String workerClassName = "";
+		int task = -1;
+		try {
+			JsonParser jParser = jfactory.createParser(content);
+			// loop until token equal to "}"
+			while (jParser.nextToken() != JsonToken.END_OBJECT) {
+		 
+				String fieldname = jParser.getCurrentName();
+				switch(fieldname){
+				case "JobId":
+					jParser.nextToken();
+					jobId = Long.valueOf(jParser.getText());
+					break;
+				case "WorkerVersion":
+					jParser.nextToken();
+					workerVersion = jParser.getText();
+					break;
+				case "WorkerURL":
+					jParser.nextToken();
+					workerURL = jParser.getText();
+					break;
+				case "WorkerClassName":
+					jParser.nextToken();
+					workerClassName = jParser.getText();
+					break;
+				case "Task":
+					jParser.nextToken();
+					task = jParser.getIntValue();
+					break;
+				case "ComeBackInSeconds":
+					return new ClientTask(jParser.getIntValue());
+					
+				}
+			  }
+			  jParser.close();
+			  
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		if(jobId == -1 || task == -1|| workerVersion.equals("") || workerURL.equals("") || workerURL.equals(""))
+			throw new IllegalStateException();
+		return new ClientTask(jobId, workerVersion, workerURL, workerClassName, task);
+	}
+
+	public void doWork() {
+		
+	}
+	
+
+}
